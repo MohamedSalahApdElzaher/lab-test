8ddadedeffaeb829db0df8799c079b2d4e3cb6a9
Muhammad Salah <47748059+MohamedSalahApdElzaher@users.noreply.github.com>
2022-03-19 01:35:30 +0200

    basis work setup

---
+++ app.js
@@ -0,0 +1,20 @@
+/**
+ * add dependency
+ */
+const express = require('express');
+const bodyparser = require('body-parser');
+
+/**
+ * use express | bodyparser | port used
+ */
+const app = express();
+app.use(bodyparser.json());
+const port = 3001;
+
+
+/**
+ * listen to port
+ */
+app.listen(port, () => {
+    console.log(`books app listening at http://localhost: ${port}`);
+});
