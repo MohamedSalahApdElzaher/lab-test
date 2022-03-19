93ac2ec16ced4ebff4b4adb283e56b1b512bfd68
Muhammad Salah <47748059+MohamedSalahApdElzaher@users.noreply.github.com>
2022-03-19 02:05:22 +0200

    basis work setup

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ const app = express();
 app.use(bodyparser.json());
 const port = 3001;
 
+/**
+ * add data object | for ex: books api
+ * {
+ *  id: number
+ *  name: string
+ *  author: string
+ * }
+ */
+let books = [
+    {
+        id:1,
+        name:"human\'s life",
+        author:"john doe",
+    },
+    {
+        id:2,
+        name:"animal\'s life",
+        author:"barker hamed",
+    },
+    {
+        id:3,
+        name:"birds\'s life",
+        author:"angela luba",
+    }
+];
+
 
 /**
  * listen to port
