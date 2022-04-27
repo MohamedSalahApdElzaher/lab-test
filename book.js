95cc1bd47162e09924ccb8d3c07ae2dae4b68f8e
reemhesh <71567506+reemhesh@users.noreply.github.com>
2022-04-27 05:43:52 +0200

    Update book.js

--- book.js
+++ book.js
@@ -6,12 +6,13 @@ const { Book } = require('./Book.controller');
 
 const bookRouter =Router();
 
-
+//to create  new book in monoogse database
 bookRouter.post('/',Book.createBook);
 
+//to delete  book in monoogse database
 bookRouter.delete('/:id' ,Book.deleteBook);
 
-bookRouter.patch('/:id',Book.updateBook);
+
 
 
 
